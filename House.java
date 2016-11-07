2633056f3fcf42011c22024d389367a6095c52a5
Aqib Rashid <Aqib Rashid>
2016-11-07 12:04:30 +0000

    Create House class

---
+++ House.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This class represents a House.
+ * 
+ * @author Aqib Rashid
+ *
+ */
+
+public class House {
+
+	private List<Seed> seedsInHouse;
+
+	/**
+	 * Creates list of of seeds
+	 */
+	public House() {
+		seedsInHouse = new ArrayList<>();
+	}
+
+	/**
+	 * Get the number of seeds in the house
+	 * 
+	 * @return the number of seeds in the house
+	 */
+	public int getCount() {
+		return seedsInHouse.size();
+	}
+
+	/**
+	 * Add a seed to the house
+	 * 
+	 */
+	public void addSeed(Seed seed) {
+		seedsInHouse.add(seed);
+	}
+}
