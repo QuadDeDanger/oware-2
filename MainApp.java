b124dd9dcfc7d01c07622d0f3fac0e830a16d5e8
Aqib Rashid <aqib.rashid@kcl.ac.uk>
2016-11-08 20:15:09 +0000

    Add some tests in main class

--- MainApp.java
+++ MainApp.java
@@ -12,13 +12,18 @@ public class MainApp {
 		Player player1 = new Player("Player 1");
 		Player player2 = new Player("Player 2");
 		Board board = new Board(player1, player2);
-		
-		
-		
+		System.out.println("Initial");
 		board.print();
-		System.out.println("after");
-		board.sow(0, 0);
-		board.print();
+		System.out.println(player1.getName() + " score: " + player1.getScore() + ", " + player2.getName() + " score: "
+				+ player2.getScore());
+
+		board.strictlyTestMakeMove(player1, 0, 0);
+		board.strictlyTestMakeMove(player2, 1, 5);
+		board.strictlyTestMakeMove(player1, 0, 4);
+		board.strictlyTestMakeMove(player2, 1, 4);
+		board.strictlyTestMakeMove(player1, 0, 0);
+		board.strictlyTestMakeMove(player2, 1, 2);
+		System.out.println("Player 2 should have score 2 and house (0,4) should be empty");
 
 	}
 
--- functionality/Board.java
+++ functionality/Board.java
@@ -17,8 +17,11 @@ public class Board {
 
 	/**
 	 * Sets up a board and initialises it
-	 * @param player1 first player
-	 * @param player2 second player
+	 * 
+	 * @param player1
+	 *            first player
+	 * @param player2
+	 *            second player
 	 */
 	public Board(Player player1, Player player2) {
 		board = new House[2][6];
@@ -37,19 +40,26 @@ public class Board {
 
 	/**
 	 * Performs the sowing action (a move)
-	 * @param i the x coordinate of the seed clicked on
-	 * @param j the y coordinate of the seed clicked on
+	 * 
+	 * @param i
+	 *            the x coordinate of the seed clicked on
+	 * @param j
+	 *            the y coordinate of the seed clicked on
 	 */
 	public void sow(int i, int j) {
-		List<Seed> toSow = board[i][j].getSeedsAndEmptyHouse(); // get the list of seeds and clear that house
+		List<Seed> toSow = board[i][j].getSeedsAndEmptyHouse(); // get the list
+																// of seeds and
+																// clear that
+																// house
 
 		House currentHouse = board[i][j]; // get the current house
 		for (int index = 0; index < toSow.size(); ++index) {
 			currentHouse = getNextHouse(currentHouse); // get the next one
 			/*
-			 * 12-seed rule: if are sowing more than 12 seeds, we don't want to replant in the starting house
+			 * 12-seed rule: if are sowing more than 12 seeds, we don't want to
+			 * replant in the starting house
 			 */
-			if (currentHouse == board[i][j]) { 
+			if (currentHouse.equals(board[i][j])) {
 				currentHouse = getNextHouse(currentHouse); // so we skip
 			}
 			currentHouse.addSeedInPot(toSow.get(index)); // sow a seed
@@ -57,11 +67,12 @@ public class Board {
 
 	}
 
-	// Get next house by checking which row. If first, we go backwards, if second we go forwards
+	// Get next house by checking which row. If first, we go backwards, if
+	// second we go forwards
 	private House getNextHouse(House house) {
 		int currentX = house.getXPos();
 		int currentY = house.getYPos();
-	
+
 		if (currentX == 0) {
 			if (currentY == 0) {
 				return board[currentX + 1][currentY];
@@ -90,13 +101,23 @@ public class Board {
 
 	}
 
+	// Strictly for debugging. This mustn't be used in the game. Remove soon!
+	public void strictlyTestMakeMove(Player player, int i, int j) {
+		System.out.println(" ");
+		sow(i, j);
+		System.out.println(player.getName() + ":  After sowing (" + i + "," + j + ")");
+		print();
+		System.out.println(player1.getName() + " score: " + player1.getScore() + ", " + player2.getName() + " score: "
+				+ player2.getScore());
+	}
+
 	// For debugging only
 	public void print() {
 		for (int i = 0; i < 2; ++i) {
 			for (int j = 0; j < 6; ++j) {
 				System.out.print(board[i][j].getCount() + " ");
 			}
-			System.out.println("\n");
+			System.out.println(" ");
 		}
 	}
 
