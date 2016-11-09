88a17ac3f8ef2c124d21901ca9e536f42db76448
Aqib Rashid <aqib.rashid@kcl.ac.uk>
2016-11-09 11:40:00 +0000

    Start Single Player

---
+++ functionality/SinglePlayer.java
@@ -0,0 +1,15 @@
+package functionality;
+
+public class SinglePlayer extends Player {
+
+	public SinglePlayer() {
+		super("Computer");
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void makeMove(){
+		
+	}
+
+	
+}
