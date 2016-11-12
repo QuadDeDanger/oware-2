334ed019f6d836eeaf43ea43178631b119f9c0bd
Aqib Rashid <aqib.rashid@kcl.ac.uk>
2016-11-12 15:45:38 +0000

    Add comments to all classes except Board and AI

--- functionality/Player.java
+++ functionality/Player.java
@@ -1,96 +1,109 @@
 package functionality;
 
 /**
- * This class represents a Player that has a Score House
- *
+ * This class represents a Player in the game. Minor changes by Aqib Rashid
+ * 
  * @author Haaris Memon
  */
 public class Player {
 
-    private String name;
-    private ScoreHouse scoreHouse;
-    private boolean isPlayersTurn;
+	private String name; // player's name
+	private ScoreHouse scoreHouse; // player's scorehouse
+	private boolean isPlayersTurn; // is true when it is player's turn
 
-    /**
-     * Create Player with a name and initialised score house
-     *
-     * @param name of the Player
-     */
-    public Player(String name) {
-        this.name = name;
-        scoreHouse = new ScoreHouse();
-    }
+	/**
+	 * Create Player with a name and initialised (empty) score house
+	 *
+	 * @param name
+	 *            of the Player
+	 */
+	public Player(String name) {
+		this.name = name;
+		scoreHouse = new ScoreHouse();
+	}
 
-    /**
-     * Sets the name of the player
-     *
-     * @param name of the Player
-     */
-    public void setName(String name) {
-        this.name = name;
-    }
+	/**
+	 * Sets the name of the player
+	 *
+	 * @param name
+	 *            of the Player
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    /**
-     * Returns the name of the Player
-     *
-     * @return name of the Player
-     */
-    public String getName() {
-        return name;
-    }
+	/**
+	 * Returns the name of the Player
+	 *
+	 * @return name of the Player
+	 */
+	public String getName() {
+		return name;
+	}
 
-    /**
-     * Returns the Score House object that the Player has
-     *
-     * @return Score House that player has
-     */
-    public ScoreHouse getHouse() {
-        return scoreHouse;
-    }
+	/**
+	 * Returns the Score House object that the Player has
+	 *
+	 * @return Score House that player has
+	 */
+	public ScoreHouse getHouse() {
+		return scoreHouse;
+	}
 
-    /**
-     * Gets the score of how many seeds the player has
-     *
-     * @return the player's score
-     */
-    public int getScore() {
-        return scoreHouse.getCount();
-    }
+	/**
+	 * Gets the score of how many seeds the player has (which is the number of
+	 * seeds in the score house)
+	 *
+	 * @return the player's score
+	 */
+	public int getScore() {
+		return scoreHouse.getCount();
+	}
 
-    /**
-     * Adds a seed to the Player's Score House
-     *
-     * @param seed to be inserted into player's score house
-     */
-    public void addSeedToHouse(Seed seed) {
-        scoreHouse.addSeed(seed);
-    }
+	/**
+	 * Adds a seed to the Player's Score House
+	 *
+	 * @param seed
+	 *            to be inserted into player's score house
+	 */
+	public void addSeedToHouse(Seed seed) {
+		scoreHouse.addSeed(seed);
+	}
 
-    /**
-     * Returns if it is Player's turn
-     *
-     * @return true if it is Player's turn
-     */
-    public boolean getIsPlayersTurn() {
-        return isPlayersTurn;
-    }
+	/**
+	 * Returns if it is Player's turn
+	 *
+	 * @return true if it is Player's turn
+	 */
+	public boolean getIsPlayersTurn() {
+		return isPlayersTurn;
+	}
 
-    /**
-     * Set if it is Player's turn
-     *
-     * @param isTurn of the Player
-     */
-    public void setIsPlayersTurn(boolean isTurn) {
-        isPlayersTurn = isTurn;
-    }
+	/**
+	 * Set if it is Player's turn
+	 *
+	 * @param isTurn
+	 *            of the Player
+	 */
+	public void setIsPlayersTurn(boolean isTurn) {
+		isPlayersTurn = isTurn;
+	}
 
-    public void clearScoreHouse(){
-    	scoreHouse.reset();
-    }
-    
-    @Override
-    public String toString(){
-    	return name;
-    }
+	/**
+	 * Resets the player's scorehouse
+	 */
+	public void clearScoreHouse() {
+		scoreHouse.reset();
+	}
+
+	/*
+	 * Method used in debugging (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return name;
+	}
 
 }
--- functionality/ScoreHouse.java
+++ functionality/ScoreHouse.java
@@ -1,9 +1,11 @@
 package functionality;
+
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * This class represents a ScoreHouse.
+ * This class represents a ScoreHouse which stores a player's seeds (and thus
+ * the score).
  * 
  * @author Aqib Rashid
  *
@@ -14,14 +16,14 @@ public class ScoreHouse {
 	private List<Seed> seedsInHouse;
 
 	/**
-	 * Creates list of seeds
+	 * Creates a ScoreHouse with an empty list of seeds
 	 */
 	public ScoreHouse() {
 		seedsInHouse = new ArrayList<>();
 	}
 
 	/**
-	 * Get the number of seeds in the house
+	 * Get the number of seeds in the house which is the score
 	 * 
 	 * @return the number of seeds in the house
 	 */
@@ -32,14 +34,18 @@ public class ScoreHouse {
 	/**
 	 * Add a seed to the house
 	 *
-	 * @param seed to add to the house
+	 * @param seed
+	 *            seed to add to the scorehouse
 	 */
 	public void addSeed(Seed seed) {
 		seedsInHouse.add(seed);
 	}
-	
-	public void reset(){
+
+	/**
+	 * Resets the scorehouse to be empty
+	 */
+	public void reset() {
 		seedsInHouse.clear();
 	}
-	
+
 }
